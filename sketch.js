b35c1ce051f50d22a38c1fb22db2ad01075b3f77
DoubleDogeDare <55456078+DoubleDogeDare@users.noreply.github.com>
2019-09-30 22:59:38 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,403 @@
+
+let C = 1
+let G = 45
+
+function setup(){
+
+ createCanvas(G*11, G*13)
+// noStroke()
+translate(1, 1)
+	//R1
+ 
+	fill(112*C, 111, 110)
+ 	rect(0, 0, G, G)
+
+ 	fill(131*C, 131, 128)
+ 	rect(G, 0, G, G)
+
+ 	fill(178*C, 176, 155)
+ 	rect(2*G, 0, G, G)
+
+ 	fill(194*C, 194, 176)
+ 	rect(3*G, 0, G, G)
+
+ 	fill(183*C, 183, 176)
+ 	rect(4*G, 0, G, G)
+
+ 	fill(142*C, 142, 140)
+ 	rect(5*G, 0, G, G)
+
+ 	fill(120*C, 124, 124)
+ 	rect(6*G, 0, G, G)
+
+ 	fill(123*C, 127, 126)
+ 	rect(7*G, 0, G, G)
+
+ 	fill(117*C, 121, 122)
+ 	rect(8*G, 0, G, G)
+
+ 	fill(97*C, 101, 106)
+ 	rect(9*G, 0, G, G)
+
+ 	//R2
+
+	fill(174*C, 175, 177)
+ 	rect(0, G*1, G, G)
+
+ 	fill(175*C, 174, 169)
+ 	rect(G, G*1, G, G)
+
+ 	fill(178*C, 176, 155)
+ 	rect(2*G, G*1, G, G)
+
+ 	fill(206*C, 207, 205)
+ 	rect(3*G, G*1, G, G)
+
+ 	fill(210*C, 211, 210)
+ 	rect(4*G, G*1, G, G)
+
+ 	fill(185*C, 185, 183)
+ 	rect(5*G, G*1, G, G)
+
+ 	fill(159*C, 159, 157)
+ 	rect(6*G, G*1, G, G)
+
+ 	fill(146*C, 145, 145)
+ 	rect(7*G, G*1, G, G)
+
+ 	fill(136*C, 136, 138)
+ 	rect(8*G, G*1, G, G)
+
+ 	fill(129*C, 128, 129)
+ 	rect(9*G, G*1, G, G)
+ 	
+ 	//R3
+
+ 	fill(155*C, 151, 133)
+ 	rect(0, G*2, G, G)
+
+ 	fill(211*C, 214, 225)
+ 	rect(G, G*2, G, G)
+
+ 	fill(183*C, 184, 194)
+ 	rect(2*G, G*2, G, G)
+
+ 	fill(191*C, 192, 195)
+ 	rect(3*G, G*2, G, G)
+
+ 	fill(211*C, 211, 210)
+ 	rect(4*G, G*2, G, G)
+
+ 	fill(210*C, 211, 214)
+ 	rect(5*G, G*2, G, G)
+
+ 	fill(209*C, 212, 214)
+ 	rect(6*G, G*2, G, G)
+
+ 	fill(161*C, 162, 164)
+ 	rect(7*G, G*2, G, G)
+
+ 	fill(119*C, 120, 127)
+ 	rect(8*G, G*2, G, G)
+
+ 	fill(107*C, 110, 118)
+ 	rect(9*G, G*2, G, G)
+
+ 	//R4
+
+ 	fill(55*C, 42, 6)
+ 	rect(0, G*3, G, G)
+
+ 	fill(136*C, 132, 109)
+ 	rect(G, G*3, G, G)
+
+ 	fill(199*C, 202, 210)
+ 	rect(2*G, G*3, G, G)
+
+ 	fill(177*C, 180, 194)
+ 	rect(3*G, G*3, G, G)
+
+ 	fill(175*C, 176, 179)
+ 	rect(4*G, G*3, G, G)
+
+ 	fill(182*C, 185, 192)
+ 	rect(5*G, G*3, G, G)
+
+ 	fill(190*C, 193, 203)
+ 	rect(6*G, G*3, G, G)
+
+ 	fill(187*C, 187, 197)
+ 	rect(7*G, G*3, G, G)
+
+ 	fill(136*C, 137, 144)
+ 	rect(8*G, G*3, G, G)
+
+ 	fill(95*C, 98, 106)
+ 	rect(9*G, G*3, G, G)
+
+ 	//R5
+
+ 	fill(51*C, 40, 15)
+ 	rect(0, G*4, G, G)
+
+ 	fill(92*C, 76, 0)
+ 	rect(G, G*4, G, G)
+
+ 	fill(155*C, 146, 100)
+ 	rect(2*G, G*4, G, G)
+
+ 	fill(194*C, 194, 176)
+ 	rect(3*G, G*4, G, G)
+
+ 	fill(164*C, 167, 182)
+ 	rect(4*G, G*4, G, G)
+
+ 	fill(166*C, 169, 182)
+ 	rect(5*G, G*4, G, G)
+
+ 	fill(152*C, 155, 162)
+ 	rect(6*G, G*4, G, G)
+
+ 	fill(131*C, 132, 138)
+ 	rect(7*G, G*4, G, G)
+
+ 	fill(114*C, 115, 117)
+ 	rect(8*G, G*4, G, G)
+
+ 	fill(99*C, 101, 101)
+ 	rect(9*G, G*4, G, G)
+
+ 	//R6
+
+	fill(61*C, 49, 9)
+ 	rect(0, G*5, G, G)
+
+ 	fill(97*C, 79, 17)
+ 	rect(G, G*5, G, G)
+
+ 	fill(73*C, 58, 0)
+ 	rect(2*G, G*5, G, G)
+
+ 	fill(95*C, 94, 89)
+ 	rect(3*G, G*5, G, G)
+
+ 	fill(157*C, 158, 170)
+ 	rect(4*G, G*5, G, G)
+
+ 	fill(185*C, 182, 175)
+ 	rect(5*G, G*5, G, G)
+
+ 	fill(123*C, 123, 115)
+ 	rect(6*G, G*5, G, G)
+
+ 	fill(95*C, 98, 95)
+ 	rect(7*G, G*5, G, G)
+
+ 	fill(103*C, 106, 97)
+ 	rect(8*G, G*5, G, G)
+
+ 	fill(81*C, 84, 76)
+ 	rect(9*G, G*5, G, G)
+
+ 	//R7
+
+ 	fill(47*C, 36, 7)
+ 	rect(0, G*6, G, G)
+
+ 	fill(44*C, 32, 0)
+ 	rect(G, G*6, G, G)
+
+ 	fill(60*C, 48, 8)
+ 	rect(2*G, G*6, G, G)
+
+ 	fill(99*C, 99, 94)
+ 	rect(3*G, G*6, G, G)
+
+ 	fill(123*C, 125, 128)
+ 	rect(4*G, G*6, G, G)
+
+ 	fill(135*C, 130, 119)
+ 	rect(5*G, G*6, G, G)
+
+ 	fill(138*C, 130, 112)
+ 	rect(6*G, G*6, G, G)
+
+ 	fill(97*C, 97, 79)
+ 	rect(7*G, G*6, G, G)
+
+ 	fill(94*C, 92, 66)
+ 	rect(8*G, G*6, G, G)
+
+ 	fill(92*C, 87, 61)
+ 	rect(9*G, G*6, G, G)
+
+ 	//R8
+
+ 	fill(15*C, 15, 10)
+ 	rect(0, G*7, G, G)
+
+ 	fill(35*C, 28, 1)
+ 	rect(G, G*7, G, G)
+
+ 	fill(82*C, 67, 19)
+ 	rect(2*G, G*7, G, G)
+
+ 	fill(96*C, 88, 48)
+ 	rect(3*G, G*7, G, G)
+
+ 	fill(98*C, 95, 78)
+ 	rect(4*G, G*7, G, G)
+
+ 	fill(115*C, 115, 109)
+ 	rect(5*G, G*7, G, G)
+
+ 	fill(120*C, 119, 108)
+ 	rect(6*G, G*7, G, G)
+
+ 	fill(100*C, 96, 67)
+ 	rect(7*G, G*7, G, G)
+
+ 	fill(96*C, 80, 25)
+ 	rect(8*G, G*7, G, G)
+
+ 	fill(81*C, 71, 30)
+ 	rect(9*G, G*7, G, G)
+
+ 	//R9
+
+ 	fill(30*C, 24, 6)
+ 	rect(0, G*8, G, G)
+
+ 	fill(13*C, 16, 12)
+ 	rect(G, G*8, G, G)
+
+ 	fill(39*C, 34, 11)
+ 	rect(2*G, G*8, G, G)
+
+ 	fill(50*C, 36, 0)
+ 	rect(3*G, G*8, G, G)
+
+ 	fill(110*C, 101, 53)
+ 	rect(4*G, G*8, G, G)
+
+ 	fill(148*C, 144, 97)
+ 	rect(5*G, G*8, G, G)
+
+ 	fill(151*C, 142, 85)
+ 	rect(6*G, G*8, G, G)
+
+ 	fill(102*C, 86, 30)
+ 	rect(7*G, G*8, G, G)
+
+ 	fill(64*C, 51, 0)
+ 	rect(8*G, G*8, G, G)
+
+ 	fill(53*C, 45, 12)
+ 	rect(9*G, G*8, G, G)
+
+ 	//R10
+
+ 	fill(55*C, 37, 2)
+ 	rect(0, G*9, G, G)
+
+ 	fill(37*C, 27, 5)
+ 	rect(G, G*9, G, G)
+
+ 	fill(15*C, 16, 9)
+ 	rect(2*G, G*9, G, G)
+
+ 	fill(17*C, 16, 7)
+ 	rect(3*G, G*9, G, G)
+
+ 	fill(20*C, 20, 10)
+ 	rect(4*G, G*9, G, G)
+
+ 	fill(35*C, 35, 13)
+ 	rect(5*G, G*9, G, G)
+
+ 	fill(75*C, 61, 6)
+ 	rect(6*G, G*9, G, G)
+
+ 	fill(72*C, 57, 0)
+ 	rect(7*G, G*9, G, G)
+
+ 	fill(46*C, 40, 3)
+ 	rect(8*G, G*9, G, G)
+
+ 	fill(39*C, 35, 9)
+ 	rect(9*G, G*9, G, G)
+
+ 	//R11
+
+ 	fill(50*C, 39, 0)
+ 	rect(0, G*10, G, G)
+
+ 	fill(74*C, 54, 4)
+ 	rect(G, G*10, G, G)
+
+ 	fill(57*C, 41, 6)
+ 	rect(2*G, G*10, G, G)
+
+ 	fill(36*C, 27, 4)
+ 	rect(3*G, G*10, G, G)
+
+ 	fill(43*C, 33, 4)
+ 	rect(4*G, G*10, G, G)
+
+ 	fill(31*C, 23, 5)
+ 	rect(5*G, G*10, G, G)
+
+ 	fill(16*C, 16, 7)
+ 	rect(6*G, G*10, G, G)
+
+ 	fill(25*C, 23, 10)
+ 	rect(7*G, G*10, G, G)
+
+ 	fill(30*C, 26, 7)
+ 	rect(8*G, G*10, G, G)
+
+ 	fill(25*C, 25, 14)
+ 	rect(9*G, G*10, G, G)
+
+ 	//R12
+
+ 	fill(60*C, 44, 5)
+ 	rect(0, G*11, G, G)
+
+ 	fill(79*C, 65, 9)
+ 	rect(G, G*11, G, G)
+
+ 	fill(83*C, 62, 5)
+ 	rect(2*G, G*11, G, G)
+
+ 	fill(75*C, 55, 3)
+ 	rect(3*G, G*11, G, G)
+
+ 	fill(99*C, 77, 10)
+ 	rect(4*G, G*11, G, G)
+
+ 	fill(79*C, 61, 7)
+ 	rect(5*G, G*11, G, G)
+
+ 	fill(26*C, 26, 16)
+ 	rect(6*G, G*11, G, G)
+
+ 	fill(20*C, 21, 19)
+ 	rect(7*G, G*11, G, G)
+
+ 	fill(21*C, 21, 17)
+ 	rect(8*G, G*11, G, G)
+
+ 	fill(18*C, 18, 19)
+ 	rect(9*G, G*11, G, G)
+
+
+
+
+print("everything is ok")
+	
+}
+
+function mouseClicked() {
+	print(int(mouseX),int(mouseY))
+}
